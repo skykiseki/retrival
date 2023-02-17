6101f7fa2b2bda96a3dc65fac741f5c436854112
skykiseki <595791833@qq.com>
2023-02-18 01:44:53 +0800

    Update search_engine.py

--- retrival/search_engine.py
+++ retrival/search_engine.py
@@ -1,4 +1,6 @@
 import os
+import jieba
+
 
 class SearchEngine(object):
     """
@@ -8,9 +10,29 @@ class SearchEngine(object):
     ----------
     index_name: str, 索引名称
 
+    stop_words: list, 停用词表
+
     """
-    def __init__(self, index_name):
+    def __init__(self, index_name, stop_words):
         self.index_name = index_name
+        self.stop_words = stop_words
+
+    def query_preprocessing(self, query):
+        """
+        query文本的预处理
+
+        Parameters:
+        ----------
+        query: str, 字符串
+
+        Returns:
+        -------
+        terms: list[str], 返回切分后的词汇列表
+
+        """
+        terms = [word for word in jieba.lcut_for_search(query) if word not in self.stop_words]
+
+        return terms
 
     def search(self, query):
         """
@@ -18,6 +40,16 @@ class SearchEngine(object):
         Parameters:
         ----------
         query: str, 字符串
+
+        Returns:
+        -------
+
+
         """
+        query_terms = self.query_preprocessing(query)
+
+        if len(query_terms) == 0:
+            return {}
+
         return 1
 
